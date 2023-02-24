952a7506c70386057948537f0e20d8233869a551
Roksana Sadeghi <roksana.s.sadeghi@gmail.com>
2023-02-24 14:05:45 -0800

    Adding a new class in SpinnakerInterface for single FLIR camera system.

---
+++ source/SpinnakerInterface/EyeTrackingSystemSpinnaker_SingleCam.cs
@@ -0,0 +1,16 @@
+﻿using OpenIris;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpinnakerInterface
+{
+    [Export(typeof(EyeTrackingSystem)), PluginDescriptionEyeTrackingSystem("Spinnaker Single Camera - RS Test", typeof(EyeTrackingSystemSettings))]
+
+    internal class EyeTrackingSystemSpinnaker_SingleCam
+    {
+    }
+}

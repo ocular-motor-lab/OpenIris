846426bd96bde3d5f5a50b58325982154e14257f
Roksana Sadeghi <roksana.s.sadeghi@gmail.com>
2023-03-28 16:37:50 -0700

    Edited and cleaned up the FindCornealReflectionsBlob in the RS function - changes include: added open and close image similar to pupiltracker, change the pupil roi to iris roi, find the corneal reflect blob within iris radius from the pupil center.

--- source/OpenIrisLib/ImageProcessing/CornealReflectionTracking.cs
+++ source/OpenIrisLib/ImageProcessing/CornealReflectionTracking.cs
@@ -7,7 +7,9 @@ namespace OpenIris.ImageProcessing
 {
     using System;
     using System.Drawing;
+    using Emgu.CV;
     using Emgu.CV.Cvb;
+    using Emgu.CV.CvEnum;
     using Emgu.CV.Structure;
 
     /// <summary>
@@ -47,14 +49,20 @@ namespace OpenIris.ImageProcessing
         /// <param name="referencePupil">Position of the pupil in the reference image.</param>
         /// <param name="trackingSettings">Settings for tracking.</param>
         /// <returns></returns>
-        public CornealReflectionData[] FindCornealReflections(ImageEye imageEye, PupilData pupilAprox, PupilData referencePupil, EyeTrackingPipelineJOMSettings trackingSettings)
+        public CornealReflectionData[] FindCornealReflections(ImageEye imageEye, PupilData pupilAprox, EyeTrackingPipelineJOMSettings trackingSettings)
         {
             if (trackingSettings is null) throw new ArgumentNullException(nameof(trackingSettings));
 
+            int blurSize = (int)Math.Ceiling(trackingSettings.MinCRRadPix / 2);
+            var threshold = (imageEye.WhichEye == Eye.Left) ? trackingSettings.BrightThresholdLeftEye : trackingSettings.BrightThresholdRightEye;
+            var maxBlobArea = trackingSettings.MaxCRRadPix * Math.PI * Math.PI;
+            var minBlobArea = trackingSettings.MinCRRadPix * Math.PI * Math.PI;
+            var irisRadiusPix =  (int) Math.Round( (imageEye.WhichEye == Eye.Left) ? trackingSettings.IrisRadiusPixLeft : trackingSettings.IrisRadiusPixRight);
+
             return trackingSettings.CornealReflectionTrackingMethod switch
             {
-                CornealReflectionTrackingMethod.Blob => FindCornealReflectionsBlob(detector, blobs, imageEye, pupilAprox, trackingSettings),
-                CornealReflectionTrackingMethod.BlobRS => FindCornealReflectionsBlobRS(detector, blobs, imageEye, pupilAprox, trackingSettings),
+                CornealReflectionTrackingMethod.Blob => FindCornealReflectionsBlob(detector, blobs, imageEye, pupilAprox, blurSize, threshold, maxBlobArea, minBlobArea),
+                CornealReflectionTrackingMethod.BlobRS => FindCornealReflectionsBlobRS(detector, blobs, imageEye, pupilAprox, blurSize, threshold, maxBlobArea, minBlobArea, irisRadiusPix),
                 _ => Array.Empty<CornealReflectionData>(),
             };
         }
@@ -66,19 +74,16 @@ namespace OpenIris.ImageProcessing
         /// <param name="blobs"></param>
         /// <param name="imageEye">Image of the eye.</param> 
         /// <param name="pupilAprox"></param>
-        /// <param name="trackingSettings">Configuration parameters.</param>
+        /// <param name="blurSize"></param>
+        /// <param name="maxBlobArea"></param>
+        /// <param name="minBlobArea"></param>
+        /// <param name="threshold"></param>
         /// <returns>Pupil ellipse.</returns>
-        public static CornealReflectionData[] FindCornealReflectionsBlob(CvBlobDetector detector, CvBlobs blobs, ImageEye imageEye, PupilData pupilAprox, EyeTrackingPipelinePupilCRSettings trackingSettings)
+        public static CornealReflectionData[] FindCornealReflectionsBlob(CvBlobDetector detector, CvBlobs blobs, ImageEye imageEye, PupilData pupilAprox, int blurSize, int threshold, double maxBlobArea, double minBlobArea)
         {
             if (detector is null) throw new ArgumentNullException(nameof(detector));
             if (blobs is null) throw new ArgumentNullException(nameof(blobs));
             if (imageEye is null) throw new ArgumentNullException(nameof(imageEye));
-            if (trackingSettings is null) throw new ArgumentNullException(nameof(trackingSettings));
-
-            int blurSize = (int)Math.Ceiling(trackingSettings.MinCRRadPix / 2);
-            var threshold = (imageEye.WhichEye == Eye.Left) ? trackingSettings.BrightThresholdLeftEye : trackingSettings.BrightThresholdRightEye;
-            var maxBlobArea = trackingSettings.MaxCRRadPix * Math.PI * Math.PI;
-            var minBlobArea = trackingSettings.MinCRRadPix * Math.PI * Math.PI;
 
             // Add some padding to the pupil ROI just in case make the ROI always square. This may
             // help in some occasions.
@@ -157,36 +162,55 @@ namespace OpenIris.ImageProcessing
         /// <param name="blobs"></param>
         /// <param name="imageEye">Image of the eye.</param> 
         /// <param name="pupilAprox"></param>
-        /// <param name="trackingSettings">Configuration parameters.</param>
+        /// <param name="blurSize"></param>
+        /// <param name="maxBlobArea"></param>
+        /// <param name="minBlobArea"></param>
+        /// <param name="threshold"></param>
+        /// <param name="irisRadiusPix"></param>
         /// <returns>Pupil ellipse.</returns>
-        public static CornealReflectionData[] FindCornealReflectionsBlobRS(CvBlobDetector detector, CvBlobs blobs, ImageEye imageEye, PupilData pupilAprox, EyeTrackingPipelinePupilCRSettings trackingSettings)
+        public static CornealReflectionData[] FindCornealReflectionsBlobRS(CvBlobDetector detector, CvBlobs blobs, ImageEye imageEye, PupilData pupilAprox, int blurSize, int threshold, double maxBlobArea, double minBlobArea, int irisRadiusPix)
         {
             if (detector is null) throw new ArgumentNullException(nameof(detector));
             if (blobs is null) throw new ArgumentNullException(nameof(blobs));
             if (imageEye is null) throw new ArgumentNullException(nameof(imageEye));
-            if (trackingSettings is null) throw new ArgumentNullException(nameof(trackingSettings));
 
-            int blurSize = (int)Math.Round(trackingSettings.MinCRRadPix / 2);
-            var threshold = (imageEye.WhichEye == Eye.Left) ? trackingSettings.BrightThresholdLeftEye : trackingSettings.BrightThresholdRightEye;
-            var maxBlobArea = trackingSettings.MaxCRRadPix * Math.PI * Math.PI;
-            var minBlobArea = trackingSettings.MinCRRadPix * Math.PI * Math.PI;
-
-            // Add some padding to the pupil ROI just in case make the ROI always square. This may
-            // help in some occasions.
-            var squareSize = Math.Max(pupilAprox.Size.Width, pupilAprox.Size.Height);
-            var roiPupil = new Rectangle(
-                 (int)Math.Round(pupilAprox.Center.X - (squareSize) - squareSize / 2),
-                 (int)Math.Round(pupilAprox.Center.Y - (squareSize / 2.0) - squareSize / 4),
-                 (int)Math.Round(squareSize * 2 + squareSize / 2 * 2),
-                 (int)Math.Round(squareSize + squareSize / 4 * 2));
-            roiPupil.Intersect(new Rectangle(new Point(0, 0), imageEye.Size));
+            //using iris radius for the ROI to look for CR
+            int squareSize = irisRadiusPix * 2;
+            var irisROI = new Rectangle(
+                (int) Math.Round(pupilAprox.Center.X - squareSize/2),
+                (int) Math.Round(pupilAprox.Center.Y - squareSize/2),
+                squareSize,
+                squareSize
+                );
+            irisROI.Intersect(new Rectangle(new Point(0, 0), imageEye.Size));
 
             ////////////////////////////////////////////////////////////////////////////////////////////////////////
             // -- Thresholding -- Get the binary image with the dark pixels
             ////////////////////////////////////////////////////////////////////////////////////////////////////////
-            var imgTemp = imageEye.Image.Copy(roiPupil).SmoothBlur(blurSize, blurSize);
+            
+            var imgTemp = imageEye.Image.Copy(irisROI).SmoothBlur(blurSize, blurSize);
             var imgPupilBinary = imgTemp.ThresholdBinary(new Gray(threshold), new Gray(255));
-            // TODO: image open and close
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // --  Morphological erosion and dilation --
+            // Optimize the blobs by removing small white or black spots
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            var kernel = CvInvoke.GetStructuringElement(ElementShape.Ellipse, new Size(5, 5), new Point(1, 1));
+            var center = new Point(-1, -1);
+            var one = new MCvScalar(1);
+
+            // Image opening. Morpholigical operation to remove small spots that are above treshold.
+            // First it erodes the image. That is reduce the size of the white blobs. Next it dilates the
+            // remaning blobs. This will eliminate the blobs that are smaller than the kernel size.
+            CvInvoke.Erode(imgPupilBinary, imgPupilBinary, kernel, center, 1, BorderType.Default, one);
+            CvInvoke.Dilate(imgPupilBinary, imgPupilBinary, kernel, center, 1, BorderType.Default, one);
+
+            // Image closing. Morpholigical operation to remove small spots that are below treshold.
+            // First it dilates the image. That is increase the size of the white blobs. Next it erods the
+            // remaning blobs. This will eliminate the spots within the blobs that are smaller than the kernel size.
+            CvInvoke.Dilate(imgPupilBinary, imgPupilBinary, kernel, center, 1, BorderType.Default, one);
+            CvInvoke.Erode(imgPupilBinary, imgPupilBinary, kernel, center, 1, BorderType.Default, one);
 
 
             ////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -202,8 +226,10 @@ namespace OpenIris.ImageProcessing
             {
                 CvBlob blob = item.Value;
 
+                var distanceToPupil = Math.Sqrt(Math.Pow(Math.Abs(blob.Centroid.X) - irisRadiusPix, 2) + Math.Pow( Math.Abs(blob.Centroid.Y) - irisRadiusPix, 2));
+
                 // If area is too small
-                if (blob.Area > maxBlobArea || blob.Area < minBlobArea)
+                if (blob.Area > maxBlobArea || blob.Area < minBlobArea || distanceToPupil > irisRadiusPix )
                 {
                     continue;
                 }
@@ -217,7 +243,7 @@ namespace OpenIris.ImageProcessing
                 if (score > 0.1 && crs.Count < 5)
                 {
                     crs.Add(new CornealReflectionData(
-                        new PointF(blob.Centroid.X + roiPupil.X, blob.Centroid.Y + roiPupil.Y),
+                        new PointF(blob.Centroid.X + irisROI.X, blob.Centroid.Y + irisROI.Y),
                         new SizeF(blob.BoundingBox.Size),
                         (float)90.0));
                 }
@@ -231,7 +257,7 @@ namespace OpenIris.ImageProcessing
                 var imgDebug = imgPupilBinary.Convert<Bgr, byte>();
                 foreach (var cr in crs)
                 {
-                    imgDebug.Draw(new CircleF(new PointF(cr.Center.X - roiPupil.X, cr.Center.Y - roiPupil.Y), cr.Size.Width * 2), new Bgr(Color.Red), 2);
+                    imgDebug.Draw(new CircleF(new PointF(cr.Center.X - irisROI.X, cr.Center.Y - irisROI.Y), cr.Size.Width * 2), new Bgr(Color.Red), 2);
                 }
 
                 EyeTrackerDebug.AddImage("CR", imageEye.WhichEye, imgDebug);
